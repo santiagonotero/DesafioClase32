782dfde92af08f17d59a58112368d5abe92063da
santiagonotero <santiagonotero@otmail.com>
2022-04-28 02:22:19 -0300

    ahora funciona nginx

--- cluster.main.js
+++ cluster.main.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
       name   : "Server 1",
       script : "main.js",
-      args   : "--PORT=8080 --mode=cluster",
+      args   : "--PORT=8081 --mode=cluster",
       instances : "max",
       exec_mode : "cluster"
     }]
--- worker.main.js
+++ worker.main.js
@@ -2,26 +2,18 @@ module.exports = {
     apps : [{
       name   : "Server 1",
       script : "main.js",
-      args   : "--PORT=8080 --mode=fork"
+      args   : "-- --PORT=8082"
     }, {
       name   : "Server 2",
       script : "main.js",
-      args   : "--PORT=8081 --mode=fork"
+      args   : "-- --PORT=8083"
     }, {
       name   : "Server 3",
       script : "main.js",
-      args   : "--PORT=8082 --mode=fork"
+      args   : "-- --PORT=8084"
     }, {
       name   : "Server 4",
       script : "main.js",
-      args   : "--PORT=8083 --mode=fork"
-    }, {
-      name   : "Server 5",
-      script : "main.js",
-      args   : "--PORT=8084 --mode=fork"
-    }, {
-      name   : "Server 6",
-      script : "main.js",
-      args   : "--PORT=8085 --mode=fork"
+      args   : "-- --PORT=8083"
     }]
   }
