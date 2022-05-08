464de24d9ce9bad4825f89364aa7b8a51f373167
santiagonotero <santiagonotero@otmail.com>
2022-05-08 11:27:37 -0300

    instalación de mongoose en package.json

--- DBconfig/Mongo.js
+++ DBconfig/Mongo.js
@@ -2,7 +2,8 @@ module.exports ={
     HOSTNAME: "cluster0.pzszz.mongodb.net",
     SCHEMA: "mongodb+srv",
     USER: "santiagonotero",
-    PASSWORD: "6Jcse5Kl9hbrH1iA",
+    PASSWORD: MONGO_PASSWORD, //
+    //PASSWORD: "6Jcse5Kl9hbrH1iA",
     DATABASE: "desafioclase22",
     OPTIONS: "retryWrites=true&w=majority"
 }
