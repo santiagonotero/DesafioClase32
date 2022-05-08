9bb3ef20f8f06f8f8d5535bf302a06627a267985
santiagonotero <santiagonotero@otmail.com>
2022-05-08 11:44:51 -0300

    corrección de la variable del password de Mongo

--- DBconfig/Mongo.js
+++ DBconfig/Mongo.js
@@ -2,7 +2,7 @@ module.exports ={
     HOSTNAME: "cluster0.pzszz.mongodb.net",
     SCHEMA: "mongodb+srv",
     USER: "santiagonotero",
-    PASSWORD: MONGO_PASSWORD, //
+    PASSWORD: process.env.MONGO_PASSWORD, //
     //PASSWORD: "6Jcse5Kl9hbrH1iA",
     DATABASE: "desafioclase22",
     OPTIONS: "retryWrites=true&w=majority"
